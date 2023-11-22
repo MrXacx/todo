f796ef81ef67463a26bf16f539c93c140af8b7ca
MrXacx <misterxacx@gmail.com>
2023-11-22 15:07:48 -0300

    feat: cria base para sign up

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using todo.Models;
+
+namespace todo.Controllers;
+
+public class SignUpController : Controller
+{
+    private readonly ILogger<SignUpController> _logger;
+
+    public SignUpController(ILogger<SignUpController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}

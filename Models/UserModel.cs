b6385c81003f1f6510275d727fbd32dde0b92a0d
Ariel Santos <misterxacx@gmail.com>
2023-11-25 11:28:58 -0300

    feat: cria model de usuário

---
+++ Models/UserModel.cs
@@ -0,0 +1,10 @@
+namespace todo.Models;
+
+public class UserModel
+{
+    private int Id {get; set;}
+    private string Name {get; set;}
+    private string Email {get; set;}
+    private string Password {get; set;}
+
+}

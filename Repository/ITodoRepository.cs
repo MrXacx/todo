62e8000030c1895477f3ce86cd10d19f60af61e1
MrXacx <misterxacx@gmail.com>
2023-11-26 17:18:36 -0300

    feat: retira List de ITodoRepository

--- Repository/ITodoRepository.cs
+++ Repository/ITodoRepository.cs
@@ -3,9 +3,8 @@ using todo.Models;
 
 interface ITodoRepository<Model> where Model : IModel // Abstrai interface para receber e retornar qualquer implementação de IModel
 {
-    Model create(Model model);
-    Model read(int Id);
-    List<Model> list(Model model);
-    Model update(Model model);
-    bool delete(Model model);
+    Model Create(Model Model);
+    Model Read(int Id);
+    Model Update(Model Model);
+    bool Delete(Model Model);
 }

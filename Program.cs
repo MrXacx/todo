8f4a433ccbd5a8b46efc1a38582e53a1a36c9373
MrXacx <misterxacx@gmail.com>
2023-11-26 18:36:44 -0300

    chore: configura uso das classes Repository

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using todo.Data;
 using Microsoft.Extensions.DependencyInjection;
+using todo.Repository;
+using todo.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
 builder.Services.AddControllers();
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultDatabase");
@@ -14,6 +13,11 @@ builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection));
 });
 
+builder.Services.AddScoped<ITodoRepository<UserModel>, UserRepository>();
+builder.Services.AddScoped<ITodoRepository<TaskModel>, TaskRepository>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

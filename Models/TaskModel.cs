fcd68806e6d230a5d2b12a9f603cf1ee7ea29224
MrXacx <misterxacx@gmail.com>
2023-11-25 23:52:34 -0300

    feat: cria model de tarefa

---
+++ Models/TaskModel.cs
@@ -0,0 +1,15 @@
+namespace todo.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class TaskModel
+{
+    public int? Id {get; set;}
+    public int? Author {get; set;}
+    public DateTime? Date {get; set;}
+    public string? Text {get; set;}
+
+}

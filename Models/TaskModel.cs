ce58d23bd918cdcfaf5029422a93452f4321d885
MrXacx <misterxacx@gmail.com>
2023-11-26 13:34:54 -0300

    feat: configura models para gerar tabelas

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -2,15 +2,29 @@ namespace todo.Models;
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Build.Framework;
 
 public class TaskModel
 {
-    public int? Id { get; set; }
-    public int? Author { get; set; }
-    public DateTime? Date { get; set; }
-    public string? Title { get; set; }
-    public string? Text { get; set; }
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(UserModel))]
+    public required int AuthorId { get; set; }
+
+    public required UserModel Author { get; set; }
+
+    public DateTime Date { get; set; } = DateTime.Now;
+
+    [MinLength(3)]
+    [MaxLength(30)]
+    public required string Title { get; set; }
+
+    [MinLength(10)]
+    [MaxLength(300)]
+    public required string Text { get; set; }
 
 }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,14 +2,26 @@ namespace todo.Models;
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
+[Index(propertyNames: ["Email"], IsUnique = true, Name = "IX_User_Email")]
 public class UserModel
 {
-    public int? Id {get; set;}
-    public string? Name {get; set;}
-    public string? Email {get; set;}
-    public string? Password {get; set;}
+    public int Id { get; set; }
 
+    [MinLength(3)]
+    [MaxLength(30)]
+    public required string Name { get; set; }
+
+
+    public required string Email { get; set; }
+
+    [MinLength(6)]
+    [MaxLength(35)]
+    public required string Password { get; set; }
+
+    public List<TaskModel> Tasks { get; set; } = new List<TaskModel>();
 }

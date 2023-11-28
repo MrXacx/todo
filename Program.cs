a86fa607c340b2b8dc38e84dee1fab383349a67c
MrXacx <misterxacx@gmail.com>
2023-11-28 09:48:32 -0300

    .

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<TodoContext>(opt =>
 builder.Services.AddScoped<ITodoRepository<UserModel>, UserRepository>();
 builder.Services.AddScoped<ITodoRepository<TaskModel>, TaskRepository>();
 builder.Services.AddScoped<ITodoRepository<BoardModel>, BoardRepository>();
-builder.Services.AddScoped<ITodoRepository<AuthorizedAcessModel>, AuthorizedAcessRepository>();
+builder.Services.AddScoped<ITodoRepository<AuthorizedAccessModel>, AuthorizedAccessRepository>();
 
 builder.Services.AddSession();
 builder.Services.AddSingleton<ITempDataProvider, SessionStateTempDataProvider>();

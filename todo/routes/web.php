b5a96f6cf199683a364a493ba4f0b76a7e357506
MrXacx <misterxacx@gmail.com>
2023-12-10 13:57:52 -0300

    feat: cria rotas

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::resource('user', UserController::class);
+Route::resource('dashboard', BoardController::class);
+Route::resource('dashboard.tasks', TaskController::class);

59a039411b0e1474855b85868183b37436e08cb8
MrXacx <misterxacx@gmail.com>
2023-12-10 14:00:43 -0300

    feat: remove código em ASP.NET

--- aspnet/Exceptions/NoContentRetrieveException.cs
+++
@@ -1,8 +0,0 @@
-namespace todo.Exceptions;
-
-public class NoContentRetrieveException : Exception
-{
-    public NoContentRetrieveException() : base() { }
-    public NoContentRetrieveException(string? message) : base(message) { }
-    public NoContentRetrieveException(string? message, Exception innerException) : base(message, innerException) { }
-}

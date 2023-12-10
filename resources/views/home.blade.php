10d3fe7d086e9b302178718f56f6ba6470fd8f8a
MrXacx <misterxacx@gmail.com>
2023-12-10 16:17:48 -0300

    feat: cria home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        }
+
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Olá, mundo!</h1>
+</body>
+
+</html>
